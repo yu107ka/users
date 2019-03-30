c02793ce71a775fab199588564752d8cc7a73b68
yu107ka <yuto271828182@gmail.com>
2019-03-30 23:21:05 +0900

    users

--- resister.php
+++ resister.php
@@ -2,22 +2,47 @@
     $pdo = new PDO("mysql:dbname=mydb;host=localhost;charset=utf8","root","", [PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]);   
 
 
-    if(isset($_POST['resister']) ){
-      if(!$_POST['name']==null){
-        $name = $_POST['name'];
+    if(isset($_POST['resister']) ){ //↓↓値の取得↓↓
+        $name = htmlentities($_POST['name'],ENT_QUOTES,"UTF-8");
         $gender = $_POST['gender'];
-        $DOB = $_POST['DOB'];
+        $month = $_POST['month'];
+        $day = $_POST['day'];
+        $year = $_POST['year'];
         $height = $_POST['height'];
         $weight = $_POST['weight'];
-          $sth = $pdo->prepare("INSERT INTO users (name,gender,DOB,height,weight) VALUES (:name,:gender,:DOB,:height,:weight)");
+        $error =0; //↑↑値の取得↑↑
+        if($name==null){
+           echo 'name is emputy.<br>';
+           $error=1; //名前が空でないか
+        }
+        if($month==null||$day==null||$year==null){
+           echo 'DOB is emputy.<br>';
+           $error=1; //生年月日が空でないか
+        }elseif(!checkdate( $month, $day, $year ) ){
+            echo 'DOB is not appropriate.<br>';
+            $error=1; //生年月日が適切か
+          }
+        if($height==null){
+           echo 'height is emputy.<br>';
+           $error=1; //身長が空でないか
+        }
+        if($weight==null){
+           echo 'weight is emputy.<br>';
+           $error=1; //体重が空でないか
+        }
+
+        if($error==0){ //↓↓ユーザーを追加↓↓
+          $sth = $pdo->prepare("INSERT INTO users (name,gender,month,day,year,height,weight) VALUES (:name,:gender,:month,:day,:year,:height,:weight)");
           $sth->bindValue(':name', $name, PDO::PARAM_STR);
           $sth->bindValue(':gender', $gender, PDO::PARAM_STR);
-          $sth->bindValue(':DOB', $DOB, PDO::PARAM_STR);
-          $sth->bindValue(':height', $height, PDO::PARAM_STR);
-          $sth->bindValue(':weight', $weight, PDO::PARAM_STR);
+          $sth->bindValue(':month', $month, PDO::PARAM_INT);
+          $sth->bindValue(':day', $day, PDO::PARAM_INT);
+          $sth->bindValue(':year', $year, PDO::PARAM_INT);
+          $sth->bindValue(':height', $height, PDO::PARAM_INT);
+          $sth->bindValue(':weight', $weight, PDO::PARAM_INT);
           $sth->execute();
           header("location: userview.php");
-         }//ユーザーを追加
+          }//↑↑ユーザーを追加↑↑
 
 }
 ?>
@@ -31,11 +56,11 @@
   <button onclick="location.href='http://localhost/userview.php'">back</button>
   <form method="post" action="">
     name:<input type="text" name="name" value=""><br>
-    gender:<input type="radio" name="gender" value="male">male
+    gender:<input type="radio" name="gender" value="male" checked="checked">male
            <input type="radio" name="gender" value="female">female<br>
-    DOB(MM/DD/YYYY):<input type="text" name="DOB" value=""><br>
-    height(cm):<input type="text" name="height" value=""><br>
-    weight(kg):<input type="text" name="weight" value=""><br>
+    DOB(MM/DD/YYYY):<input type="number" max='12' min='1' name="month" value="">/<input type="number"  max='31' min='1' name="day" value="">/<input type="number" max='2019' min='1900' name="year" value=""><br>
+    height(cm):<input type="number" name="height"max='250' min='1' value=""><br>
+    weight(kg):<input type="number" name="weight"max='250' min='1' value=""><br>
     <input type="submit" name="resister" value="resister" >
   </form>
 </body>
--- update.php
+++ update.php
@@ -1,23 +1,49 @@
 <?php 
     $pdo = new PDO("mysql:dbname=mydb;host=localhost;charset=utf8","root","", [PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]);
-if(isset($_POST['update']) ){
-     $name = $_POST['name'];
+
+if(isset($_POST['update']) ){ //↓↓値の取得↓↓
+     $name = htmlentities($_POST['name'],ENT_QUOTES,"UTF-8");
      $gender = $_POST['gender'];
-     $DOB = $_POST['DOB'];
+     $month = $_POST['month'];
+     $day = $_POST['day'];
+     $year = $_POST['year'];
      $height = $_POST['height'];
      $weight = $_POST['weight'];
-     $id = $_POST['id'];
-     $sth = $pdo->prepare("UPDATE users set name =:name,gender =:gender,DOB=:DOB,height=:height,weight=:weight where id = :id");                 
+     $id = $_POST['id']; //↑↑値の取得↑↑
+    
+     if($name==null){
+           echo 'name is emputy.<br>';
+           $error=1; //名前が空でないか
+        }
+     if($month==null||$day==null||$year==null){
+           echo 'DOB is emputy.<br>';
+           $error=1; //生年月日が空でないか
+        }elseif(!checkdate( $month, $day, $year ) ){
+            echo 'DOB is not appropriate.<br>';
+            $error=1; //生年月日が適切か
+        }
+     if($height==null){
+           echo 'height is emputy.<br>';
+           $error=1; //身長が空でないか
+        }
+     if($weight==null){
+           echo 'weight is emputy.<br>';
+           $error=1; //体重が空でないか
+        } 
+     if($error==0){ //↓↓ユーザーを更新↓↓
+          $sth = $pdo->prepare("UPDATE users set name =:name,gender =:gender,month=:month,day=:day,year=:year,height=:height,weight=:weight where id = :id"); 
           $sth->bindValue(':name', $name, PDO::PARAM_STR);
           $sth->bindValue(':gender', $gender, PDO::PARAM_STR);
-          $sth->bindValue(':DOB', $DOB, PDO::PARAM_STR);
-          $sth->bindValue(':height', $height, PDO::PARAM_STR);
-          $sth->bindValue(':weight', $weight, PDO::PARAM_STR);
-          $sth->bindValue(':id', $id, PDO::PARAM_STR);
+          $sth->bindValue(':month', $month, PDO::PARAM_INT);
+          $sth->bindValue(':day', $day, PDO::PARAM_INT);
+          $sth->bindValue(':year', $year, PDO::PARAM_INT);
+          $sth->bindValue(':height', $height, PDO::PARAM_INT);
+          $sth->bindValue(':weight', $weight, PDO::PARAM_INT);
+          $sth->bindValue(':id', $id, PDO::PARAM_INT);
           $sth->execute();
           header("location: userview.php");
-    }//データの更新
-
+    }//↑↑ユーザーを更新↑↑
+}
 $id = $_GET['id'];//IDの引継ぎ
 
 ?>
@@ -53,9 +79,11 @@ $id = $_GET['id'];//IDの引継ぎ
 <?php
 }
 ?>
-    DOB(MM/DD/YYYY):<input type="text" name="DOB" value="<?= htmlspecialchars($row['DOB']) ?> "><br>
-    height(cm):<input type="text" name="height" value="<?= htmlspecialchars($row['height']) ?>"><br>
-    weight(kg):<input type="text" name="weight" value="<?= htmlspecialchars($row['weight']) ?>"><br>
+    DOB(MM/DD/YYYY):<input type="number" name="month"max='12' min='1' value="<?= htmlspecialchars($row['month']) ?>">/
+                    <input type="number" name="day" max='31' min='1'value="<?= htmlspecialchars($row['day']) ?>">/
+                    <input type="number" name="year"max='2019' min='1900' value="<?= htmlspecialchars($row['year']) ?>"><br>
+    height(cm):<input type="number" name="height"max='250' min='1' value="<?= htmlspecialchars($row['height']) ?>"><br>
+    weight(kg):<input type="number" name="weight" max='250' min='1'value="<?= htmlspecialchars($row['weight']) ?>"><br>
     <input type="hidden" name="id" value="<?= htmlspecialchars($row['id']) ?>" >     <input type="submit" name="update" value="update" >
   </form>
 </tr>
